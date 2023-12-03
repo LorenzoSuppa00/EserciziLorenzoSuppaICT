9e576f57b49454a87e278120d8ba5e57ca2a82e0
Lorenzo <suppa.lorenzo.h00@outlook.it>
2023-12-03 14:50:48 +0100

    Correzione esercizi 1-2

--- src/esercizi_da_1_a_16/Esercizi_numeri.java
+++ src/esercizi_da_1_a_16/Esercizi_numeri.java
@@ -2,61 +2,63 @@ package esercizi_da_1_a_16;
 
 import java.util.Random;
 import java.util.Scanner;
-import java.util.random.*;
-import java.util.Scanner;
 public class Esercizi_numeri {
 
 	public static void main(String[] args) {
 
 //		ESERCIZIO 1
-
-//		int x = 7;
+//
+//		Scanner scan = new Scanner(System.in);
+//		
+//		System.out.println("Dimmi un numero");
+//		int x = scan.nextInt();
 //		if(x % 2 == 0) {
 //			System.out.println("Il numero è pari");
 //		}
 //		else {
 //			System.out.println("Il numero è dispari");
 //		}
+//		scan.close();
 		
 //		ESERCIZIO 2
-		
-//		int somma = 9 + 89 + 2 + 13 + 37;
+//		Scanner scan = new Scanner(System.in);
+//		System.out.println("Dimmi 5 numeri");
+//		int somma = scan.nextInt() + scan.nextInt() + scan.nextInt() + scan.nextInt() + scan.nextInt();
 //		System.out.println("Somma: " + somma);
+//		scan.close();
 		
 //		ESERCIZIO 3 
-/*		
- * 		Funziona ma non capisco perchè devo scrivere 2 volte 
+		
+/* 		Funziona ma non capisco perchè devo scrivere 2 volte 
  * 		il numero nella console per farmi dire se un valore è troppo basso o troppo alto,
  * 		solo al primo numero che scrivo nella console funziona al primo invio.
- * 
- *
  */
 		
-		Scanner scan = new Scanner(System.in);
-		Random rand = new Random();
-		int casuale = rand.nextInt(1, 100);
-		System.out.println(casuale);
-		
-		int tentativo; // Perchè non int tentativo = scan.nextInt(); direttamente qui?
-		
-		do {
-			System.out.println("Dimmi un numero da 1 a 100.");
-			tentativo = scan.nextInt();
-			if (tentativo < casuale) {
-				System.out.println("Valore troppo basso");
-				scan.nextInt();
-			}
-			else if (tentativo > casuale) {
-				System.out.println("Valore troppo alto");
-				scan.nextInt();
-			}
-			else {
-				System.out.println("Valore corretto");
-				
-			}
-			
-		} while (tentativo != casuale);
-		scan.close();
+//		Scanner scan = new Scanner(System.in);
+//		Random rand = new Random();
+//		int casuale = rand.nextInt(1, 100);
+//		System.out.println(casuale);
+//		
+//		int tentativo; // Perchè non int tentativo = scan.nextInt(); direttamente qui?
+//		
+//		do {
+//			System.out.println("Dimmi un numero da 1 a 100.");
+//			tentativo = scan.nextInt();
+//			if (tentativo < casuale) {
+//				System.out.println("Valore troppo basso");
+//				scan.nextInt();
+//			}
+//			else if (tentativo > casuale) {
+//				System.out.println("Valore troppo alto");
+//				scan.nextInt();
+//			}
+//			else {
+//				System.out.println("Valore corretto");
+//				
+//			}
+//			
+//		} while (tentativo != casuale);
+//		scan.close();
 		
 //		ESERCIZIO 4 (NON RIUSCITO)
 		
